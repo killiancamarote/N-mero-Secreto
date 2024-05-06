a248cf087e7ca928166aee42c9bfcc1432e86ca0
killiancamarote <kilobomi@gmail.com>
2024-05-06 12:46:50 -0300

    ATUALIZADO-Projeto-Jogo-Número-Secreto-Site

---
+++ app.js
@@ -0,0 +1,61 @@
+// let titulo = document.querySelector("h1");
+// titulo.innerHTML = "Jogo do número secreto";
+
+// let paragrafo = document.querySelector("p");
+// paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+
+let numeroSecreto  = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela("h1", "Jogo do Número Secreto");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+
+function verificarChute() {
+    let chute = document.querySelector("input").value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela("h1", "Acertou!");
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela("p", mensagemTentativas);
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela("p", "O número secreto é menor");
+        } else {
+            exibirTextoNaTela("p", "O número secreto é maior");
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    let campo = document.querySelector("input");
+    campo.value = '';
+}
+
+// function limparCampo() {
+//     let campo = document.querySelector("input");
+//     campo.value = '';
+//     }
+
+
+// function limparCampo() {
+//     chute = document.querySelector("input");
+//     chute.value = '';
+// }
+
+// function limparCampo() {
+//     let campo = document.getElementById("campoDeTexto");
+//     campo.value = '';
+// }
+
